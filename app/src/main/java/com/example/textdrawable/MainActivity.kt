143677e8f93ff3c7e0cb303cbaacbe6d5e3e7cfd
Your Name <you@example.com>
2020-12-12 17:08:02 +0800

    把重新设置Drawable大小的函数改成扩展函数

--- app/src/main/java/com/example/textdrawable/MainActivity.kt
+++ app/src/main/java/com/example/textdrawable/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.textdrawable
 import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewTreeObserver
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,37 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         percentTextView = findViewById(R.id.percentTv)
+        percentTextView.resetDrawableSize()
     }
-    fun textViewReSize(){
-        val drawables = percentTextView.compoundDrawables
-        val viewWidth = percentTextView.measuredWidth
-        val viewHeight = percentTextView.measuredHeight
-        val drawable = drawables[1]
-        val height = (viewHeight * 0.8f).toInt()
-        val width = (viewHeight * 0.8f).toInt()
-        val top = /*drawable.intrinsicHeight / 2 - height / 2*/ 0
-        val left = drawable.intrinsicWidth / 2 - width / 2
-        drawable.bounds.set(Rect(left,top,left + width,top + height))
-        drawable.invalidateSelf()
-        percentTextView.compoundDrawablePadding = width
-    }
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus){
-            textViewReSize()
-        }
-    }
-    fun textViewReSize1(){
-        val drawables = percentTextView.compoundDrawables
-        val viewWidth = percentTextView.measuredWidth
-        val viewHeight = percentTextView.measuredHeight
-        val drawable = drawables[1]
-        val height = (viewHeight * 0.8f).toInt()
-        val width = (viewHeight * 0.8f).toInt()
-        val top = 0
-        val left = 0
-        drawable.bounds.set(Rect(left,top,left + width,top + height))
-        percentTextView.compoundDrawablePadding = width
-        drawable.invalidateSelf()
+    fun TextView.resetDrawableSize(){
+        this.viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener{
+            override fun onPreDraw(): Boolean {
+                val drawables = compoundDrawables
+                val viewWidth = measuredWidth
+                val viewHeight = measuredHeight
+                val drawable = drawables[1]
+                val height = (viewHeight * 0.7f).toInt()
+                val width = (viewHeight * 0.7f).toInt()
+                val top = /*drawable.intrinsicHeight / 2 - height / 2*/ 0
+                val left = drawable.intrinsicWidth / 2 - width / 2
+                drawable.bounds.set(Rect(left,top,left + width,top + height))
+                compoundDrawablePadding = width
+                drawable.invalidateSelf()
+                viewTreeObserver.removeOnPreDrawListener(this )
+                return true
+            }
+        })
     }
 }
